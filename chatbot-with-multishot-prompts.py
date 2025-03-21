4cb6256f1f254032b6f3a82eefb789c7f2d82f4c
Deepak Singhal <singhal03.deepak@gmail.com>
2025-03-21 16:01:41 -0700

    Update and rename chatbot.py to chatbot-with-multishot-prompts.py

--- chatbot-with-multishot-prompts.py
+++ chatbot-with-multishot-prompts.py
@@ -28,7 +28,15 @@ openai.api_key=os.getenv('OPENAI_API_KEY')
 #             ["user message3", "assistant response3"],]
 
 def chat(message, history):
-    messages = [{"role": "system", "content": "You are a helpful assistant"}]
+    system_message="You are a helpful assistant in a clothes store. You should try to gently encourage \
+the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. \
+For example, if the customer says 'I'm looking to buy a hat', \
+you could reply something like, 'Wonderful - we have lots of hats - including several that are part of our sales event.'\
+Encourage the customer to buy hats if they are unsure what to get."
+    system_message += "\nIf the customer asks for shoes, you should respond that shoes are not on sale today, \
+but remind the customer to look at hats!"
+    
+    messages = [{"role": "system", "content": system_message}]
 
     # Transform history into the correct format
     for user_msg, assistant_msg in history:
