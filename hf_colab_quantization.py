29db1b3e1697606ad2805b8409bb0b58cd880562
Deepak Singhal <singhal03.deepak@gmail.com>
2025-04-03 00:23:24 -0700

    Update hf_colab_quantization.py

--- hf_colab_quantization.py
+++ hf_colab_quantization.py
@@ -20,9 +20,6 @@ messages = [
     {"role":"user", "content":"Tell a light-hearted joke"}
 ]
 
-
-######################################################### LLAMA #############################################################
-
 # Quantization Config - this allows us to load the model into memory and use less memory
 quant_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -31,20 +28,22 @@ quant_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4"
 )
 
-# Tokenizer
-tokenizer = AutoTokenizer.from_pretrained(LLAMA)
-tokenizer.pad_token = tokenizer.eos_token
-inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+def generate(model, messages):
+  # Tokenizer
+  tokenizer = AutoTokenizer.from_pretrained(model)
+  tokenizer.pad_token = tokenizer.eos_token
+  inputs = tokenizer.apply_chat_template(messages, return_tensors="pt", add_generation_prompt=True).to("cuda")
+  streamer = TextStreamer(tokenizer)
+  # Model
+  model = AutoModelForCausalLM.from_pretrained(model, device_map="auto", quantization_config=quant_config)
+  # Generate Output
+  outputs = model.generate(inputs, max_new_tokens=80, streamer=streamer)
+  # CleanUp
+  del tokenizer, streamer, model, inputs, outputs
+  torch.cuda.empty_cache()
 
-# Model
-model=AutoModelForCausalLM.from_pretrained(LLAMA, device_map="auto", quantization_config = quant_config)
-
-memory = model.get_memory_footprint() / 1e6
-print(f"Memory footprint: {memory:,.1f} MB")
-print(model)
-outputs = model.generate(inputs, max_new_tokens=80)
-print(tokenizer.decode(outputs[0]))
-
-# Clean up
-del inputs, outputs, model
-torch.cuda.empty_cache()
+generate(LLAMA, messages)
+# generate(PHI3, messages)
+# generate(GEMMA2, messages)
+# generate(QWEN2, messages)
+# generate(MIXTRAL, messages)
