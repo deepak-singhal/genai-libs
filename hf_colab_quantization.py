588adae5f5c51087c06d8fa0e490ea618600ae60
Deepak Singhal <singhal03.deepak@gmail.com>
2025-04-02 21:32:33 -0700

    Create hf_colab_quantization.py

---
+++ hf_colab_quantization.py
@@ -0,0 +1,50 @@
+import torch
+from google.colab import userdata
+from huggingface_hub import login
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer, BitsAndBytesConfig
+
+# Login to HuggingFace with Token stored in Google Colab config
+hf_token = userdata.get("HF_TOKEN")
+login(hf_token)
+
+# LLM Models - Instruct
+LLAMA = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+PHI3 = "microsoft/Phi-3-mini-4k-instruct"
+GEMMA2 = "google/gemma-2-2b-it"
+QWEN2 = "Qwen/Qwen2-7B-Instruct" # exercise for you
+MIXTRAL = "mistralai/Mixtral-8x7B-Instruct-v0.1" # If this doesn't fit it your GPU memory, try others from the hub
+
+# Messages
+messages = [
+    {"role":"system", "content":"You are an helpful assistant"},
+    {"role":"user", "content":"Tell a light-hearted joke"}
+]
+
+
+######################################################### LLAMA #############################################################
+
+# Quantization Config - this allows us to load the model into memory and use less memory
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4"
+)
+
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(LLAMA)
+tokenizer.pad_token = tokenizer.eos_token
+inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+
+# Model
+model=AutoModelForCausalLM.from_pretrained(LLAMA, device_map="auto", quantization_config = quant_config)
+
+memory = model.get_memory_footprint() / 1e6
+print(f"Memory footprint: {memory:,.1f} MB")
+print(model)
+outputs = model.generate(inputs, max_new_tokens=80)
+print(tokenizer.decode(outputs[0]))
+
+# Clean up
+del inputs, outputs, model
+torch.cuda.empty_cache()
